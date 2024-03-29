96887ac77aa6bd56cfa9ff7221a62d1dfb7a2d6d
Juan12331 <juanxadez2@gmail.com>
2024-03-29 11:24:24 -0300

    adicionando index.html

---
+++ objeto.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')
+
+// a programação orientada a objeto é formada por 4 estruturas mestres: classes, objetos, atributos e métodos
+
+//classe é um conjunto de objetos  que se definem pelo fato desses objetos terem uma mou mais caracteristicas em comum
+//objeto é qualquer coisa instanciada a partir de uma classe (é um elemento pertencente a classe)
