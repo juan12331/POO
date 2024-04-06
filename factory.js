ca54177372a4043ff1ba49f34b13ce19e415641e
Juan12331 <juanxadez2@gmail.com>
2024-04-06 11:26:55 -0300

    criando personagem homem e mulher

--- factory.js
+++ factory.js
@@ -5,15 +5,20 @@
 //factory:
 //criar uma factory para poder criar varios personagens
 
-function createCar() { //criando uma função que retorna um objeto
+function createPerson(sexo = "homem") //se não passar nada da variavel sexo ela vai valer o valor "homem"
+{ //criando uma função que retorna um objeto
     let grana = 5 // quando eu chamo grana aqui não é preciso usar "personagem.grana" para se referir a grana do usuario
     return {
-        sexo: "homem",
+        sexo: sexo,
         nome: "juan",
-        grana: 5,
+        grana, //quando a propriedade é do mesmo valor que a variavel você pode deixar a estrutura assim ao inves de grana: grana
         trabalhar: function () {
             grana += 2
             return console.log(`agora você tem ${grana} reais`)
         }
     }
 }
+
+const homem = createPerson("homem") // criando personagem homem
+const mulher = createPerson("mulher") //criando personagem mulher
+console.log()
