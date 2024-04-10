155d2bc4d2a9af528c2588a56ab94b65a2c2a96e
juan12331 <146020152+juan12331@users.noreply.github.com>
2024-04-10 11:07:00 -0300

    Update factory.js

--- factory.js
+++ factory.js
@@ -13,8 +13,12 @@ function createPerson(sexo = "homem") //se não passar nada da variavel sexo ela
         nome: "juan",
         grana, //quando a propriedade é do mesmo valor que a variavel você pode deixar a estrutura assim ao inves de grana: grana
         trabalhar: function () {
+            if (sexo == "mulher") {
+                return console.log("mulher é incapaz de trabalhar")
+            } else{
             grana += 2
             return console.log(`agora você tem ${grana} reais`)
+            }
         }
     }
 }
@@ -24,6 +28,8 @@ const mulher = createPerson("mulher") //criando personagem mulher
 console.log(homem.sexo) // vai retornar homem pois la encima colocamos que o sexo = homem
 console.log(mulher.sexo) // vai retornar homem pois la encima colocamos que o sexo = mulher
 
+homem.trabalhar
+mulher.trabalhar
 //caso queiramos que alguma propriedade do objeto exista (como o sexo) mas que ela não seja mostrada
 //podemos usar o factory (retirando apenas o sexo do return) e declarando como uma variavel fora do return
 //o que é impossivel de fazer no objectLiteral
