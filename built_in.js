26f3ea9daee7cb6fbbbe4ae862882d045ad0fb53
Juan12331 <juanxadez2@gmail.com>
2024-04-14 12:08:06 -0300

    built-in

--- built_in.js
+++ built_in.js
@@ -15,9 +15,11 @@ console.log(Math.round(x)) // arredonda
 
 //DATE
 
+
+
 console.log(Date()) // pega a data atual
 
-setTimeout(function() {
+let timeOut = setTimeout(function() {
 
     console.log("teste")
 
@@ -26,3 +28,22 @@ setTimeout(function() {
 //vale ressaltar que o setTimeout é assincrono ou seja, o javascript vai passar reto pelo código só esperando o tempo de 1000 milisegundos
 console.log('console.log') // esse console.log aparece antes do setTimeOut mesmo sendo escrito depois
 
+let intervalos = 1
+
+let intervalo = setInterval(function() { // é assincrona também
+    
+    if (intervalos >= 2) {
+        clearInterval(intervalo) // limpa os intervalos quando chega a 2 ou mais intervalos
+        console.log("fim dos intervalos")
+    }
+
+    console.log('intervalo')
+    intervalos = intervalos + 1
+    console.log(intervalos)
+}, 1500 /* <-- loop a cada 1 segundo */)
+
+//clear time out
+
+if (1 === 1) {
+    clearTimeout(timeOut) // se 1 for igual a 1 ele não vai mais mostrar o time out
+}
