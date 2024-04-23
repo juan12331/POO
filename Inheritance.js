232e053c50a326ef961e318a1fcc36875cc427b4
Juan12331 <juanxadez2@gmail.com>
2024-04-23 20:25:12 -0300

    criando classes

--- Inheritance.js
+++ Inheritance.js
@@ -21,3 +21,13 @@
 //aqui podemos ver 2 problamas gravissimos, que seriam a duplicação de código, e apossibilidade da função der um bug (teriamos que arrumar e em todos)
 //por isso em javascript (e na maioria das linguagens) existe uma regra que diz que se você escreve o exato mesmo código duas vezes existe um jeito melhor de fazer ele
 //ou seja aquele código la encima, é mal feito
+//Poderiamos resolver esse problema por exemplo nesse caso usando factory, mas dessa vez usaremos inherancite
+
+class Pessoa {
+    falar() {
+        return 'blablabla';
+    }
+}
+
+const eu = new Pessoa();
+const voce = new Pessoa();
