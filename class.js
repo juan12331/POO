eca164c5c427cdb2a24153a56b812e7d5aab3c9f
juan12331 <146020152+juan12331@users.noreply.github.com>
2024-04-25 14:41:28 -0300

    Update class.js

--- class.js
+++ class.js
@@ -1,24 +1,21 @@
 class humano {
-  nome
-  idade
-  sexo
-
-constructor(nome, idade, sexo) {
-  this.nome= nome
-  this.idade = idade
-  this.sexo = sexo
+    constructor(nome, idade, sexo) {
+    this.nome= nome
+    this.idade = idade
+    this.sexo = sexo
+  }
+  
+  apresentacao() {
+    if (this.sexo == "homem") { return `Olá eu sou o ${this.nome} e tenho ${this.idade} anos`}
+    else if (this.sexo == "mulher") { return `Olá eu sou a ${this.nome} e tenho ${this.idade} anos` }
+    else { return `Olá eu sou ${this.nome} e tenho ${this.idade} anos` }
+  }
 }
-
-apresentacao() {
-  if (this.sexo == "homem") { return `Olá eu sou o ${this.nome} e tenho ${this.idade} anos`}
-  else if (this.sexo == "mulher") { return `Olá eu sou a ${this.nome} e tenho ${this.idade} anos` }
-  else { return `Olá eu sou ${this.nome} e tenho ${this.idade} anos` }
-}
-
-let homem = new humano("juan", 15, "homem")
-let alexandre = new humano("alexandre", 16, undefined)
-let mulher = new humano("clara", 13, "mulher")
-
-console.log(homem.apresentacao())
-console.log(alexandre.apresentacao())
-console.log(mulher.apresentacao())
+  
+  let homem = new humano("juan", 15, "homem")
+  let sla = new humano("sla", 16, undefined)
+  let mulher = new humano("clara", 13, "mulher")
+  
+  console.log(homem.apresentacao())
+  console.log(sla.apresentacao())
+  console.log(mulher.apresentacao())
